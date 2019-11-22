713bbabceab5b3ca2f0a4f968ff1475b55b36321
XuanrZhang <xzhangxmu@gmail.com>
2019-11-22 21:12:50 +1100

    run script

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#--- build index----
+./bwa-mem2 index ref.fa;
+
+#--- map by BWA-MEM ---
+./bwa-mem2 mem -T 90 ref.fa reads.fq > gene.sam;
+
+awk '{if(NR>2 && $6=="100M") print $1" "$2 " "$4 " "$10}' gene.sam |sort -nk3 > extract.sam;
+
+#--- run correction----
+a="--start_poi=${start_poi}";
+b="--end_poi=${end_poi}";
+c="--sam_file=extract.sam"; 
+d="--write_file=corrected.fa"; 
+e="--write_file_2in=corrected_2in.txt";
+f="--write_file_seq=contigs.fa";
+python error_cor.py $a $b $c $d $e $f;
+
