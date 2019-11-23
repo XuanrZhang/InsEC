c01bb75bd44ec3edf3e2aca0aa7ed1a50929e5c4
XuanrZhang <xzhangxmu@gmail.com>
2019-11-23 17:37:08 +1100

    Run InsEC

--- run.sh
+++ run.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-
-#--- install BWA-MEM ---
-git clone https://github.com/bwa-mem2/bwa-mem2
-cd bwa-mem2
-make
-cd ..
+echo "given sequence: $1"
+echo "given read dataset: $2"
+echo "the length of the given sequence: $3"
+echo "the length of the read: $4"
+echo "start read extraction"
 
 #--- build index----
-bwa-mem2/bwa-mem2 index ref.fa 
+./bwa-mem2 index $1 
 
 #--- map by BWA-MEM ---
-bwa-mem2/bwa-mem2 mem -T 90 ref.fa reads.fq > gene.sam;
-awk '{if(NR>2 && $6=="100M") print $1" "$2 " "$4 " "$10}' gene.sam |sort -nk3 > extract.sam;
+./bwa-mem2 mem -T 90 $1 $2 > gene.sam;
+awk 'BEGIN{tmp="'"${4}M"'"}{if(NR>2 && $6==tmp) print $1" "$2 " "$4 " "$10}' gene.sam |sort -nk3 > extract.sam;
 
 #--- run correction----
-a="--start_poi=${start_poi}";
-b="--end_poi=${end_poi}";
-c="--sam_file=extract.sam"; 
-d="--write_file=corrected.fa"; 
-e="--write_file_2in=corrected_2in.txt";
-f="--write_file_seq=contigs.fa";
-python error_cor.py $a $b $c $d $e $f;
-
+a="--gene_L=${3}";
+b="--read_L=${4}";
+echo "python error_cor.py $a $b";
+python error_cor.py $a $b;
