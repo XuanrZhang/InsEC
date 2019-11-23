1966e9efac09fc1f0bde706faa0d0096410a7767
XuanrZhang <xzhangxmu@gmail.com>
2019-11-23 18:08:17 +1100

    Shell to run InsEC

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ echo "the length of the read: $4"
 echo "start read extraction"
 
 #--- build index----
-./bwa-mem2 index $1 
+bwa-mem2-2.0pre1_x64-linux/bwa-mem2 index $1 
 
 #--- map by BWA-MEM ---
-./bwa-mem2 mem -T 90 $1 $2 > gene.sam;
+bwa-mem2-2.0pre1_x64-linux/bwa-mem2 mem -T 90 $1 $2 > gene.sam;
 awk 'BEGIN{tmp="'"${4}M"'"}{if(NR>2 && $6==tmp) print $1" "$2 " "$4 " "$10}' gene.sam |sort -nk3 > extract.sam;
 
 #--- run correction----
