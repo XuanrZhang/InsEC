902b1bcf3c46c8fa871621ca523a089e17f61289
XuanrZhang <xzhangxmu@gmail.com>
2019-11-25 10:41:46 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-echo "given sequence: $1"
-echo "given read dataset: $2"
-echo "the length of the given sequence: $3"
-echo "the length of the read: $4"
 echo "start read extraction"
+
+fL=$[$(awk '{if(NR>1)print}' ${1}|wc -m)-$(awk '{if(NR>1)print}' ${1}|wc -l)-100]
+rL=$[$(awk '{if(NR==2)print$1}' ${2}|wc -m)-1]
 
 #--- build index----
 bwa-mem2-2.0pre1_x64-linux/bwa-mem2 index $1 
 
 #--- map by BWA-MEM ---
 bwa-mem2-2.0pre1_x64-linux/bwa-mem2 mem -T 90 $1 $2 > gene.sam;
-awk 'BEGIN{tmp="'"${4}M"'"}{if(NR>2 && $6==tmp) print $1" "$2 " "$4 " "$10}' gene.sam |sort -nk3 > extract.sam;
+awk 'BEGIN{tmp="'"${rL}M"'"}{if(NR>2 && $6==tmp) print $1" "$2 " "$4 " "$10}' gene.sam |sort -nk3 > extract.sam;
 
 #--- run correction----
-a="--gene_L=${3}";
-b="--read_L=${4}";
+a="--gene_L=${fL}";
+b="--read_L=${rL}";
 echo "python error_cor.py $a $b";
 python error_cor.py $a $b;
